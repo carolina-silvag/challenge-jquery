89fe95022362fc2797df71d653d5d2b66e7012d6
carolina.silvag <carolinasguzman@gmail.com>
2018-01-08 19:32:12 -0300

    paso 8 listo

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,7 +1,21 @@
 $(document).ready( function(){
 
-	console.log('listo para usar')
+	console.log('listo para usar');
 });
 
 /*ocultar menu*/
 $('.js-menu').hide();
+
+/*agregar make cuando click*/
+$('.js-show-make').click(appendMake);
+
+function appendMake() {
+	$('.page').addClass('make');
+}
+
+/*quitar make cuando click*/
+$('.js-show-recipe').click(removeMake);
+
+function removeMake() {
+	$('.page').removeClass('make');
+}
